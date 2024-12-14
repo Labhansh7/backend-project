b53e5bc030c85aa1947afbac0c0b850cdee3ceba
labhansh <labhansh107@gmail.com>
2024-12-14 18:36:18 +0530

    add comment, plalist ,tweet,like models

---
+++ src/models/like.model.js
@@ -0,0 +1,28 @@
+import { TokenExpiredError } from "jsonwebtoken";
+import mongoose, { Schema } from "mongoose";
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const likeSchema = new Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    commnet: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const like = mongoose.model("Like", likeSchema);
